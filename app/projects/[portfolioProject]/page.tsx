1809ee31f4469dd30b59d8de3c02bb580821d17c
Myrsini Ioannou <you@example.com>
2023-08-30 16:38:23 +0300

    running tests

--- app/projects/[portfolioProject]/page.tsx
+++ app/projects/[portfolioProject]/page.tsx
@@ -1,4 +1,4 @@
-import fs from "fs";
+//import { promises as fs } from 'fs'
 import path from "path";
 import matter from "gray-matter";
 import Link from "next/link";
@@ -6,46 +6,46 @@ import Carousel from "../../components/Carousel";
 import Video from "../../components/Video";
 
 
+import fs from "fs";
+const getProjectContent = (project: string) => {
+  const directoryPath = path.join(process.cwd(), "public/portfolio/", project);
+  const files = fs.readdirSync(directoryPath);
+  const projectData = {
+    title: "",
+    subtitle: "",
+    date: "",
+    materials: "",
+    content: "",
+    images: [] as string[],
+  };
 
-// const getProjectContent = (project: string) => {
-//   const directoryPath = path.join("public/portfolio/", project);
-//   const files = fs.readdirSync(directoryPath);
-//   const projectData = {
-//     title: "",
-//     subtitle: "",
-//     date: "",
-//     materials: "",
-//     content: "",
-//     images: [] as string[],
-//   };
-
-//   files.forEach((file) => {
-//     const filePath = path.join(directoryPath, file);
-//     if (file.endsWith(".md")) {
-//       const fileContent = fs.readFileSync(filePath, { encoding: "utf8" });
-//       let fileDataParsed = matter(fileContent);
-//       projectData.title = fileDataParsed.data.title;
-//       projectData.subtitle = fileDataParsed.data.subtitle;
-//       projectData.date = fileDataParsed.data.date;
-//       projectData.materials = fileDataParsed.data.materials;
-//       projectData.content = fileDataParsed.content;
-//     } else {
-//       projectData.images.push(file);
-//     }
-//   });
-//   return projectData;
-// };
+  files.forEach((file) => {
+    const filePath = path.join(directoryPath, file);
+    if (file.endsWith(".md")) {
+      const fileContent = fs.readFileSync(filePath, { encoding: "utf8" });
+      let fileDataParsed = matter(fileContent);
+      projectData.title = fileDataParsed.data.title;
+      projectData.subtitle = fileDataParsed.data.subtitle;
+      projectData.date = fileDataParsed.data.date;
+      projectData.materials = fileDataParsed.data.materials;
+      projectData.content = fileDataParsed.content;
+    } else {
+      projectData.images.push(file);
+    }
+  });
+  return projectData;
+};
 
 const projectPage = (props: any) => {
   const project = props.params.portfolioProject;
-  //const content = getProjectContent(project);
+  const content = getProjectContent(project);
 
   return (
     <div className="flex-1 flex-col h-full overflow-y-auto">
       <Link href="/" className="flex font-bold text-3xl pt-6">
         &#8592;
       </Link>
-      {/* <div className="">
+      <div className="">
         <h1 className="flex font-bold text-5xl pt-12 py-6">{content.title}</h1>
         <h2 className="flex font-bold text-lg">{content.subtitle}</h2>
         <p className="flex font-bold text-lg">{content.date}</p>
@@ -55,7 +55,7 @@ const projectPage = (props: any) => {
       <div className="">
         <Carousel project={project} images={content.images}></Carousel>
       </div>
-      <div className="pt-12 xl:pt-24 ">
+      {/* <div className="pt-12 xl:pt-24 ">
         <Video projecTitle={content.title}></Video>
       </div> */}
     </div>
@@ -66,3 +66,59 @@ const projectPage = (props: any) => {
 export default projectPage;
 
 
+
+
+// export async function getStaticProps() {
+//   const postsDirectory = path.join(process.cwd(), 'posts')
+//   const filenames = await fs.readdir(postsDirectory)
+ 
+//   const posts = filenames.map(async (filename) => {
+//     const filePath = path.join(postsDirectory, filename)
+//     const fileContents = await fs.readFile(filePath, 'utf8')
+ 
+ 
+//     return {
+//       filename,
+//       content: fileContents,
+//     }
+//   })
+//   // By returning { props: { posts } }, the Blog component
+//   // will receive `posts` as a prop at build time
+//   return {
+//     props: {
+//       posts: await Promise.all(posts),
+//     },
+//   }
+// }
+
+
+
+
+// const projectPage = (props: any) => {
+//   const project = props.params.portfolioProject;
+//   //const content = getProjectContent(project);
+
+//   return (
+//     <div className="flex-1 flex-col h-full overflow-y-auto">
+//       <Link href="/" className="flex font-bold text-3xl pt-6">
+//         &#8592;
+//       </Link>
+//       {/* <div className="">
+//         <h1 className="flex font-bold text-5xl pt-12 py-6">{content.title}</h1>
+//         <h2 className="flex font-bold text-lg">{content.subtitle}</h2>
+//         <p className="flex font-bold text-lg">{content.date}</p>
+//         <p className="flex font-bold text-lg">Materials: {content.materials}</p>
+//       </div>
+//       <p className="py-8">{content.content}</p>
+//       <div className="">
+//         <Carousel project={project} images={content.images}></Carousel>
+//       </div>
+//       <div className="pt-12 xl:pt-24 ">
+//         <Video projecTitle={content.title}></Video>
+//       </div> */}
+//     </div>
+//   );
+// };
+
+
+// export default projectPage;
